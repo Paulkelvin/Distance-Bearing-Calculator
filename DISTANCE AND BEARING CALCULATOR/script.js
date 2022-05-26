14e231135b82591d22be291ecc4abc22876c9c6a
Paulkelvin <Ibukunadesanya0@gmail.com>
2022-05-26 15:22:19 +0100

    complete-distance-bearing-calculator

---
+++ DISTANCE AND BEARING CALCULATOR/script.js
@@ -0,0 +1,42 @@
+'strict mode';
+
+// const p1E = Number(document.querySelector('.point-one-easting').value);
+// const p1N = Number(document.querySelector('.point-one-northing').value);
+// const p2E = Number(document.querySelector('.point-two-easting').value);
+// const p2N = Number(document.querySelector('.point-two-northing').value);
+// console.log(p2N, typeof p2N);
+// const result = document.querySelector('.result');
+
+const calculate = document.querySelector('.calculate');
+
+calculate.addEventListener('click', function (e) {
+  e.preventDefault();
+  const p1E = Number(document.querySelector('.point-one-easting').value);
+  const p1N = Number(document.querySelector('.point-one-northing').value);
+  const p2E = Number(document.querySelector('.point-two-easting').value);
+  const p2N = Number(document.querySelector('.point-two-northing').value);
+  const result = document.querySelector('.result');
+
+  const calculateDistance = function (X1, Y1, X2, Y2) {
+    const distance = Math.sqrt(
+      Math.pow(X2 - X1, 2) + Math.pow(Y2 - Y1, 2)
+    ).toFixed(4);
+
+    let bearing = Math.atan((X2 - X1) / (Y2 - Y1)) * (180 / Math.PI);
+
+    const degree = Math.floor(bearing);
+    const decimalMinute = (bearing - degree) * 60;
+    const minute = Math.floor(decimalMinute);
+    const seconds = ((decimalMinute - minute) * 60).toFixed(4);
+
+    bearing = `${degree}° ${minute}' ${seconds}"`;
+    if (p1E === 0 || p1N === 0 || p2E === 0 || p2N === 0) {
+      result.textContent = '';
+      alert('You must input all value');
+    } else {
+      result.textContent = `Distance: ${distance}m Bearing: ${bearing}`;
+    }
+  };
+  calculateDistance(p1E, p1N, p2E, p2N);
+  // calculateDistance(531970.793, 2790620.373, 532013.829, 2790645.827);
+});
